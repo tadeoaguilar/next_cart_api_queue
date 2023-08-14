f8f71c7a97306a3ed64e61fa7d59572bc79785be
Tadeo Aguilar <Tadeo.Aguilar@FINNING.COM>
2023-08-14 18:23:44 -0400

    azure-func

---
+++ next_cart_queue/index.ts
@@ -0,0 +1,20 @@
+import { AzureFunction, Context } from "@azure/functions"
+
+const queueTrigger: AzureFunction = async function (context: Context, myQueueItem: string): Promise<void> {
+    context.log('Queue trigger function processed work item', myQueueItem);
+    if (myQueueItem) {
+        //context.bindings.cosmosDocument = JSON.stringify({
+            context.bindings.cosmosDocument = {
+            // create a random ID
+            id: new Date().toISOString() + Math.random().toString().substring(2, 10),
+            queue: {
+                other:myQueueItem,
+                myobj:2
+            },
+            try:1
+        };
+
+    }
+};
+
+export default queueTrigger;
